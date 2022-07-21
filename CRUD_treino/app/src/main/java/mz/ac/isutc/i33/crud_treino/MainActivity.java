9fda74f770bd64668ad3231fcc399a598e5a26c1
MarioJambo <mjambo01@gmail.com>
2022-07-21 09:03:59 +0200

    criei o método de listar de dados( o anterior cria tabela**)

--- CRUD_treino/app/src/main/java/mz/ac/isutc/i33/crud_treino/MainActivity.java
+++ CRUD_treino/app/src/main/java/mz/ac/isutc/i33/crud_treino/MainActivity.java
@@ -2,18 +2,25 @@ package mz.ac.isutc.i33.crud_treino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase bancoDados;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listView = findViewById(R.id.listaDados);
         criarBancoDados();
         listarDados();
     }
@@ -35,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void listarDados() {
+        try {
+            bancoDados = openOrCreateDatabase("CrudApp", MODE_PRIVATE, null);
+            Cursor cursor = bancoDados.rawQuery("SELECT id, nome FROM pessoas",null);//Criação de cursor
+            ArrayList <String> linhas = new ArrayList<>();
+            ArrayAdapter <String>adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, linhas);
+            listView.setAdapter(adapter);
+            cursor.moveToFirst();
+            while (cursor!=null){
+                    linhas.add(cursor.getString(1));
+                    cursor.moveToNext();
+            }
 
+        }catch (Exception exception){
+            exception.printStackTrace();
+
+        }
     }
 }
